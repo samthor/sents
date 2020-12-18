f2bb44e2a68e3542d4ca417d39198f33d04963a8
Sam Thorogood <sam.thorogood@gmail.com>
2020-12-18 11:03:45 +1100

    fix and test for inverted case-sensitive condition

--- test/test.js
+++ test/test.js
@@ -3,31 +3,65 @@ import build from '../lib/index.js';
 import fs from 'fs';
 import path from 'path';
 
-const {pathname: root} = new URL('./tmp', import.meta.url);
+const {pathname: tmpdir} = new URL('./tmp', import.meta.url);
 
-let w;
-const queue = [];
+const wait = async (timeout = 0) => {
+  await new Promise((r) => setTimeout(r, timeout));
+  await new Promise((r) => setImmediate(r));
+};
 
-test.beforeEach(() => {
-  if (w) {
-    w.close();
-  }
+const cleanup = [];
+let rootCount = 0;
 
-  fs.rmdirSync(root, {recursive: true});
+test.after(() => {
+  cleanup.forEach((c) => c());
+});
+
+fs.rmSync(tmpdir, {recursive: true, force: true});
+try {
+  fs.readdirSync(tmpdir);
+  throw new Error('tmpdir should not exist');
+} catch (e) {
+  // ok
+}
+
+const createWatcher = () => {
+  // Always use a unique directory as macOS seems to do odd things with literally just deleted
+  // folders.
+  const root = path.join(tmpdir, `root${++rootCount}_${(Math.random() * 256).toString(16)}`);
   fs.mkdirSync(root, {recursive: true});
-  queue.splice(0, queue.length);
 
-  w = build(root);
+  const queue = [];
+  const w = build(root);
   w.on('change', (filename, type) => {
     queue.push({filename, type});
   });
-});
+
+  cleanup.push(() => w.close());
+
+  return {root, w, queue};
+}
 
 test('create folder', async t => {
+  const {queue, root} = createWatcher();
+
   fs.mkdirSync(path.join(root, 'blah'));
-  await new Promise((r) => setTimeout(r, 0));
-
-  // TODO: queue update event
-
+  await wait();
   t.deepEqual(queue, [{type: 'add', filename: 'blah/'}]);
+});
+
+test('rename to same but case-sensitive', async t => {
+  const {queue, root} = createWatcher();
+
+  fs.mkdirSync(path.join(root, 'sens_what'));
+  await wait();
+  t.deepEqual(queue, [{type: 'add', filename: 'sens_what/'}], 'should just add single folder');
+  queue.splice(0, queue.length);
+
+  fs.renameSync(path.join(root, 'sens_what'), path.join(root, 'SENS_WHAT'));
+  await wait(100);
+  t.deepEqual(queue, [
+    {type: 'delete', filename: 'sens_what/'},
+    {type: 'add', filename: 'SENS_WHAT/'},
+  ], 'should add and delete old folder');
 });
