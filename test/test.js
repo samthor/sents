3ccc20c644bb5bca6ada34735b7411317987d5e8
Sam Thorogood <sam.thorogood@gmail.com>
2020-12-18 12:07:56 +1100

    update test code

--- test/test.js
+++ test/test.js
@@ -3,13 +3,6 @@ import build from '../lib/index.js';
 import fs from 'fs';
 import path from 'path';
 
-const {pathname: tmpdir} = new URL('./tmp', import.meta.url);
-
-const wait = async (timeout = 0) => {
-  await new Promise((r) => setTimeout(r, timeout));
-  await new Promise((r) => setImmediate(r));
-};
-
 const cleanup = [];
 let rootCount = 0;
 
@@ -17,6 +10,8 @@ test.after(() => {
   cleanup.forEach((c) => c());
 });
 
+// Use a known temp dir for all tests. Nuke it before start and then we use random paths inside it.
+const {pathname: tmpdir} = new URL('./tmp', import.meta.url);
 fs.rmSync(tmpdir, {recursive: true, force: true});
 try {
   fs.readdirSync(tmpdir);
@@ -25,105 +20,169 @@ try {
   // ok
 }
 
+class TestWatcherContext {
+  #w;
+  #root = '';
+  #queue = [];
+
+  constructor(root) {
+    const queue = [];
+  
+    this.#root = root;
+    this.#queue = queue;
+
+    this.#w = build(root);
+    this.#w.on('change', (filename, type) => queue.push({filename, type}));
+  }
+
+  cleanup = () => {
+    this.#w.close();
+  };
+
+  /**
+   * Clear the queue for next time.
+   */
+  clear() {
+    this.#queue.splice(0, this.#queue.length);
+  }
+
+  /**
+   * Returns a copy of the queue for checking.
+   *
+   * @return {{filename: string, type: string}[]}
+   */
+  get queue() {
+    return this.#queue.slice();
+  }
+
+  /**
+   * Wait for a number of events to arrive in the queue, or the timeout to hit.
+   *
+   * @param {number=} size to watch for resolving with true
+   * @param {number=} delay to wait resolving with false
+   * @return {Promise<boolean>}
+   */
+  wait(size = 1, delay = 1000) {
+    let cleanup;
+
+    const p = new Promise((r) => {
+      const checker = () => {
+        if (this.#queue.length >= size) {
+          r(true);
+        }
+      };
+      setTimeout(() => r(false), delay);
+
+      this.#w.on('change', checker);
+      cleanup = () => this.#w.off('change', checker);
+      checker();
+    });
+
+    return p.finally(cleanup);
+  }
+
+  /**
+   * Find a path in the test folder.
+   *
+   * @param {string} x
+   * @param {...string} rest
+   * @return {string}
+   */
+  t(x, ...rest) {
+    return path.join(this.#root, x, ...rest);
+  }
+}
+
 const createWatcher = () => {
   // Always use a unique directory as macOS seems to do odd things with literally just deleted
   // folders.
   const root = path.join(tmpdir, `root${++rootCount}_${(Math.random() * 256).toString(16)}`);
   fs.mkdirSync(root, {recursive: true});
 
-  const queue = [];
-  const w = build(root);
-  w.on('change', (filename, type) => queue.push({filename, type}));
-
-  cleanup.push(() => w.close());
-
-  return {root, w, queue};
-}
+  const watcher = new TestWatcherContext(root);
+  cleanup.push(watcher.cleanup);
+  return watcher;
+};
 
 test('create folder', async t => {
-  const {queue, root} = createWatcher();
+  const ctx = createWatcher();
 
-  fs.mkdirSync(path.join(root, 'blah'));
-  await wait();
-  t.deepEqual(queue, [{type: 'add', filename: `blah${path.sep}`}]);
+  fs.mkdirSync(ctx.t('blah'));
+  await ctx.wait();
+  t.deepEqual(ctx.queue, [{type: 'add', filename: `blah${path.sep}`}], 'simple folder creation');
 });
 
 test('file doesn\'t have trailing slash', async t => {
-  const {queue, root} = createWatcher();
+  const ctx = createWatcher();
 
-  fs.writeFileSync(path.join(root, 'testFile'), 'hi');
-  await wait();
-  t.deepEqual(queue, [{type: 'add', filename: 'testFile'}], 'file is added');
-  queue.splice(0, queue.length);
+  fs.writeFileSync(ctx.t('testFile'), 'hi');
+  await ctx.wait();
+  t.deepEqual(ctx.queue, [{type: 'add', filename: 'testFile'}], 'file is added');
+  ctx.clear();
 
-  fs.writeFileSync(path.join(root, 'testFile'), 'update');
-  await wait(100);
-  t.deepEqual(queue, [{type: 'change', filename: 'testFile'}], 'file is changed');
-  queue.splice(0, queue.length);
+  fs.writeFileSync(ctx.t('testFile'), 'update');
+  await ctx.wait();
+  t.deepEqual(ctx.queue, [{type: 'change', filename: 'testFile'}], 'file is changed');
+  ctx.clear();
 
-  fs.rmSync(path.join(root, 'testFile'), {force: true});
-  await wait(100);
-  t.deepEqual(queue, [{type: 'delete', filename: 'testFile'}], 'should delete');
+  fs.rmSync(ctx.t('testFile'), {force: true});
+  await ctx.wait();
+  t.deepEqual(ctx.queue, [{type: 'delete', filename: 'testFile'}], 'should delete');
 });
 
 test('supports dup files on case-sensitive fs', async t => {
-  const {queue, root} = createWatcher();
-  fs.writeFileSync(path.join(root, 'testFile'), 'hi');
+  const ctx = createWatcher();
+  fs.writeFileSync(ctx.t('testFile'), 'hi');
 
   try {
-    fs.statSync(path.join(root, 'TESTfile'));
+    fs.statSync(ctx.t('TESTfile'));
     return t.assert(true, 'nothing to do, case-insensitive fs');
   } catch (e) {
     // doesn't exist, test can run
   }
 
-  fs.writeFileSync(path.join(root, 'testFILE'), 'other file');
-  await wait();
+  fs.writeFileSync(ctx.t('testFILE'), 'other file');
+  await ctx.wait(2);
 
-  t.deepEqual(queue, [
-    {type: 'add', filename: `testFile`},
+  t.deepEqual(ctx.queue, [
     {type: 'add', filename: `testFILE`},
+    {type: 'add', filename: `testFile`},
   ], 'should add two files');
 });
 
 test('supports hard-link notification', async t => {
-  const {queue, root} = createWatcher();
+  const ctx = createWatcher();
 
-  fs.writeFileSync(path.join(root, 'testFile1'), 'hi');
-  fs.linkSync(path.join(root, 'testFile1'), path.join(root, 'testFile2'));
+  fs.writeFileSync(ctx.t('testFile1'), 'hi');
+  fs.linkSync(ctx.t('testFile1'), ctx.t('testFile2'));
 
-  await wait();
-  t.deepEqual(queue, [
+  await ctx.wait(2);
+  t.deepEqual(ctx.queue, [
     {type: 'add', filename: 'testFile1'},
     {type: 'add', filename: 'testFile2'},
   ], 'files are added');
-  queue.splice(0, queue.length);
+  ctx.clear();
 
-  fs.appendFileSync(path.join(root, 'testFile2'), '\nand stuff');
-  await wait(100);
+  fs.appendFileSync(ctx.t('testFile2'), '\nand stuff');
+  await ctx.wait(2);
 
-  const expected = [
+  t.deepEqual(ctx.queue, [
     {type: 'change', filename: 'testFile1'},
     {type: 'change', filename: 'testFile2'},
-  ];
-  if (queue.length === 4) {
-    // Sometimes the event arrives twice.
-    expected.push(...expected);
-  }
-  t.deepEqual(queue, expected, 'files are both changed');
+  ], 'files are both changed');
 });
 
 test('rename to same but different case', async t => {
-  const {queue, root} = createWatcher();
+  const ctx = createWatcher();
 
-  fs.mkdirSync(path.join(root, 'sens_what'));
-  await wait();
-  t.deepEqual(queue, [{type: 'add', filename: `sens_what${path.sep}`}], 'should just add single folder');
-  queue.splice(0, queue.length);
+  fs.mkdirSync(ctx.t('sens_what'));
+  await ctx.wait();
+  t.deepEqual(ctx.queue, [{type: 'add', filename: `sens_what${path.sep}`}], 'should just add single folder');
+  ctx.clear();
 
-  fs.renameSync(path.join(root, 'sens_what'), path.join(root, 'SENS_WHAT'));
-  await wait(100);
-  t.deepEqual(queue, [
+  fs.renameSync(ctx.t('sens_what'), ctx.t('SENS_WHAT'));
+  await ctx.wait(2);
+  t.deepEqual(ctx.queue, [
     {type: 'delete', filename: `sens_what${path.sep}`},
     {type: 'add', filename: `SENS_WHAT${path.sep}`},
   ], 'should add and delete old folder');
